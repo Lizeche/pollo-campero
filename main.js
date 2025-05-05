88c49ff494de6006552f59672a87e040d5980e5c
Lizeche <lisbethecheverria96@gmail.com>
2025-05-04 22:33:31 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let menu_desplegable = document.getElementById('menu');
-let boton_cerrar = document.getElementById('x');
-
-function abrir_cerrar_menu(){
-    menu_desplegable.classList.toggle('abrir_menu');
-    boton_cerrar.classList.toggle('colocar_x');
-}
